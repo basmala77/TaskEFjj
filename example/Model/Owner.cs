e9d95a9ef0520809a7fa7f0b28e5052ce01d97e7
basmala77 <basmalaelshabrawy37@gmail.com>
2024-12-01 23:01:31 +0200

    first commit

---
+++ example/Model/Owner.cs
@@ -0,0 +1,11 @@
+﻿namespace example.Model
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gym { get; set; }
+        public Country Country { get; set; }
+
+    }
+}
---
+++ example/Model/Reviewer.cs
@@ -0,0 +1,11 @@
+﻿namespace example.Model
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+
+    }
+}
